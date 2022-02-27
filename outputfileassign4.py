14d0c2cfa105fc945f49783bb4758d378be67024
coolpacific08 <96660802+coolpacific08@users.noreply.github.com>
2022-02-27 18:13:57 +0530

    Add files via upload

---
+++ outputfileassign4.py
@@ -0,0 +1,72 @@
+C:\Users\PRASHANT\AppData\Local\Programs\Python\Python310\python.exe C:/Users/PRASHANT/PycharmProjects/assignment4/codefileassign4.py
+***********************************ASSIGNMENT 4***********************************
+
+This is solution of Question 1:
+Move disk 1 from source A to destination B
+Move disk 2 from source A to destination C
+Move disk 1 from source B to destination C
+Move disk 3 from source A to destination B
+Move disk 1 from source C to destination A
+Move disk 2 from source C to destination B
+Move disk 1 from source A to destination B
+
+This is solution of Question 2:
+Enter the number of rows in Pascal's triangle: 5
+Using iterations.
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1
+
+Using recursions.
+        1
+      1   1
+    1   2   1
+  1   3   3   1
+1   4   6   4   1
+
+This is solution of Question 3:
+Enter the first integer: 6
+Enter the second integer: 4
+the remainder is 2 and quotient is 1.
+
+part a)
+Function is callable.
+
+part b)
+Both values are non zero
+
+part b)
+{6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+Immutable set:  frozenset({8, 5, 6, 7})
+Largest value in the set:  8
+Hash value:  8
+
+This is solution of Question 4:
+Object created
+The name of the student it Prashant Kumar Singh and SID is 21105041.
+Data deleted
+
+This is solution of Question 5:
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+
+This is solution of Question 6:
+Enter any word that comes to your mind.
+senorita
+Write a new word with the same letters.
+rosinate
+
+Congratulations, You have a good friendship.
+
+
+
+
+
